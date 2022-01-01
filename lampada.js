21d63637b0fb2ad9bc95e1b1bc0c3c7369733c78
Victor Santos <vfreitass16@icloud.com>
2022-01-01 00:32:02 -0300

    projeto finalizado

---
+++ lampada.js
@@ -0,0 +1,36 @@
+const ascender = document.getElementById('btnAscender')
+const apagar = document.getElementById('btnApagar')
+
+const img = document.getElementById('imgLampada')
+
+var menssagem = document.getElementById('menssagem')
+menssagem.textContent = 'Double Click na lampada para quebra-la!'
+
+
+var quebrada = false
+
+function ligarLampada() {
+    if (quebrada === false) {
+        img.setAttribute('src', './img/lampadaAcesa.png')
+    }
+}
+
+function desligarLampada() {
+    if (quebrada === false) {
+        img.setAttribute('src', './img/lampadaApagada.png')
+    }
+}
+
+function quebrarLampada() {
+    img.setAttribute('src', './img/lampadaQuebrada.png');
+    quebrada = true
+    menssagem.textContent = 'A lampada esta quebrada, recarregue a pagina para concertar!'
+    ascender.setAttribute('disabled', 'true')
+    apagar.setAttribute('disabled', 'true')
+
+
+}
+
+ascender.addEventListener('click', ligarLampada)
+apagar.addEventListener('click', desligarLampada)
+img.addEventListener('dblclick', quebrarLampada)
