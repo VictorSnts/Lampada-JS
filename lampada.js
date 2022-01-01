23eb595a0716e0a44a6de9ed2b93a0e6427332dd
Victor Santos <vfreitass16@icloud.com>
2022-01-01 01:05:46 -0300

    Alterando codigo para mudar botao dinamicamente, para acender ou apagar a lampada

--- lampada.js
+++ lampada.js
@@ -1,5 +1,4 @@
-const ascender = document.getElementById('btnAscender')
-const apagar = document.getElementById('btnApagar')
+const acao = document.getElementById('btnAcao')
 
 const img = document.getElementById('imgLampada')
 
@@ -8,29 +7,42 @@ menssagem.textContent = 'Double Click na lampada para quebra-la!'
 
 
 var quebrada = false
+var ligada = true
+
+function executaAcao() {
+    if (ligada) {
+        desligarLampada()
+    } else {
+        ligarLampada()
+    }
+}
 
 function ligarLampada() {
     if (quebrada === false) {
         img.setAttribute('src', './img/lampadaAcesa.png')
     }
+    ligada = true
+    acao.value = 'Apagar'
+
 }
 
 function desligarLampada() {
     if (quebrada === false) {
         img.setAttribute('src', './img/lampadaApagada.png')
     }
+    ligada = false
+    acao.value = 'Acender'
 }
 
 function quebrarLampada() {
     img.setAttribute('src', './img/lampadaQuebrada.png');
     quebrada = true
     menssagem.textContent = 'A lampada esta quebrada, recarregue a pagina para concertar!'
-    ascender.setAttribute('disabled', 'true')
-    apagar.setAttribute('disabled', 'true')
+    acao.setAttribute('disabled', 'true')
+    ligada = false
 }
 
-ascender.addEventListener('click', ligarLampada)
-apagar.addEventListener('click', desligarLampada)
+acao.addEventListener('click', executaAcao)
 
 img.addEventListener('mouseover', ligarLampada)
 img.addEventListener('mouseleave', desligarLampada)
